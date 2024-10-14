e49a7f3390fa356c43ef3c29122f3f5636c3f0d1
Sifat1308 <skauranand1308@gmail.com>
2024-10-13 23:08:34 -0400

    final commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/comp3123'; 
+const MONGODB_URI = 'mongodb+srv://sifatanand13:gdeo6E2tW8VznrJE@cluster1.knby9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1'; 
 
 const connectDB = async () => {
     try {
